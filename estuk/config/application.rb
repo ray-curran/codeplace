25ccc6cc48614afa991be3ce67d4e8f5a677861f
Ray Curran <rpcurr@gmail.com>
2016-09-19 13:04:41 -0500

    Images are dynamic from s3

--- estuk/config/application.rb
+++ estuk/config/application.rb
@@ -15,6 +15,8 @@ module Estuk
     config.active_record.raise_in_transactional_callbacks = true
     config.paperclip_defaults = { 
         storage: :s3,
+        s3_region: 'us-west-2',
+        s3_host_name: 's3-us-west-2.amazonaws.com',
         s3_credentials:{
             bucket: ENV['AWS_BUCKET'],
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
