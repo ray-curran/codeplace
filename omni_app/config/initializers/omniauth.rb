8a4133b4cbe45c47b6f4dc546e81846206e8a9a9
Ray Curran <rpcurr@gmail.com>
2016-09-22 14:41:56 -0500

    Finish authentication for github and twitter

--- omni_app/config/initializers/omniauth.rb
+++ omni_app/config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :facebook, '1808727469369334', 'd9624a1feae39a29e701589a7a31b61f', scope: 'user_birthday, user_friends', info_fields: 'id, email, friends', display: 'popup'
+  provider :facebook, '', '', scope: 'user_birthday, user_friends', info_fields: 'id, email, friends', display: 'popup'
+  provider :github,   '', ''
+  provider :twitter, '', ''
 end
