79e80e857292dc9d89925a9c92f4c0a92256a42e
Ray Curran <rpcurr@gmail.com>
2016-09-22 15:49:18 -0500

    User profile add

--- tinstuk/app/models/user.rb
+++ tinstuk/app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
 
+  has_attached_file :avatar, 
+                  :storage  => :s3, 
+                  :styles => { :medium => "370x370", :thumb => "100x100" },
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def self.sign_in_from_omniauth(auth)
     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
   end
 
   def self.create_user_from_omniauth(auth)
         create(
-          image: process_uri(auth['info']['image'] + "?width=9999"),
+          avatar: process_uri(auth['info']['image'] + "?width=9999"),
           email: auth['info']['email'],
           provider: auth['provider'],
           uid: auth['uid'],
@@ -17,5 +22,13 @@ class User < ApplicationRecord
           bio: auth['extra']['raw_info']['bio']
         )
   end  
+
+  private
+
+  def self.process_uri(uri)
+      image_url = URI.parse(uri)
+      image_url.scheme = 'https'
+      image_url.to_s
+  end
 
 end
--- tinstuk/db/schema.rb
+++ tinstuk/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922203252) do
+ActiveRecord::Schema.define(version: 20160922204529) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -19,11 +19,15 @@ ActiveRecord::Schema.define(version: 20160922203252) do
     t.string   "email"
     t.string   "gender"
     t.date     "date_of_birth"
-    t.string   "interest",      default: "Both"
+    t.string   "interest",            default: "Both"
     t.string   "location"
     t.string   "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
