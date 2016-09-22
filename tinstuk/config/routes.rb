5a6474f136ed61e518a0723d6eb19f4fb6dd9b69
Ray Curran <rpcurr@gmail.com>
2016-09-22 16:24:57 -0500

    Add data to views

--- tinstuk/config/routes.rb
+++ tinstuk/config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   match 'sign_out', to: 'sessions#destroy', via: :delete  
 
+  post    'create_friendships' => 'friendships#create'
+  delete  'destroy_friendships' => 'friendships#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
