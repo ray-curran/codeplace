0868b96fd105dd039ae69abfd0cc5a2c04115076
Ray Curran <rpcurr@gmail.com>
2016-09-22 15:13:38 -0500

    Layout page design

--- tinstuk/app/assets/javascripts/application.js
+++ tinstuk/app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require jquery-easing.min
+//= require jasny-bootstrap
+//= require supersized
 //= require_tree .
--- tinstuk/config/routes.rb
+++ tinstuk/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+  
+  get 'home/index'
+
+  get 'users/index'
+
+  get 'users/edit'
+
+  get 'users/profile'
+
+  get 'users/matches'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
