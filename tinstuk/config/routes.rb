63e4940e0e72cda85cf91e7b0c20d8b3e526fb8c
Ray Curran <rpcurr@gmail.com>
2016-09-22 15:42:50 -0500

    Add facebook sign in

--- tinstuk/config/routes.rb
+++ tinstuk/config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  match 'sign_out', to: 'sessions#destroy', via: :delete  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
