cc1eb6f5422f66ff8f7bd323e2d23101fa288586
Ray Curran <rpcurr@gmail.com>
2016-09-28 13:03:15 -0500

    Add Raty for ratings

--- stukplaces/db/schema.rb
+++ stukplaces/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928174638) do
+ActiveRecord::Schema.define(version: 20160928175650) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20160928174638) do
     t.integer  "user_id"
     t.integer  "place_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
