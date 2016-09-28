cd26105f035ac1858470b7ef4f58a097690030b7
Ray Curran <rpcurr@gmail.com>
2016-09-28 13:56:16 -0500

    Add post

--- stukbook/db/schema.rb
+++ stukbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928185224) do
+ActiveRecord::Schema.define(version: 20160928185528) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "trackable_type"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20160928185224) do
     t.datetime "friended_at"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.text     "content_html"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
