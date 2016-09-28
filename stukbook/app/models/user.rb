daab87bcd12f88ab09108b08439db39793be07fc
Ray Curran <rpcurr@gmail.com>
2016-09-28 13:50:18 -0500

    Make routes for friendship actions

--- stukbook/app/models/user.rb
+++ stukbook/app/models/user.rb
@@ -43,4 +43,8 @@ class User < ApplicationRecord
     end
   end
 
+  def friendship_relation(user_2)
+    Friendship.where(user_id: [self.id,user_2.id], friend_id: [self.id,user_2.id]).first
+  end
+
 end
