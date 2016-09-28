ad9ec9f06434e2a77e678537d77ae91ab2a8b932
Ray Curran <rpcurr@gmail.com>
2016-09-28 12:01:01 -0500

    Make scraper work

--- scraper/app/models/scrape.rb
+++ scraper/app/models/scrape.rb
@@ -2,9 +2,9 @@ class Scrape
   attr_accessor :title, :hotness, :image_url, :rating, :director,
   :genre, :runtime, :synopsis, :failure
 
-  def scrape_new_movie
+  def scrape_new_movie(url)
     begin
-      doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/the_martian/"))
+      doc = Nokogiri::HTML(open(url))
       doc.css('script').remove 
       self.title = doc.at_css('#movie-title').text.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
       self.hotness = doc.at_css('.meter-value').text.to_i
@@ -24,20 +24,20 @@ class Scrape
     end
   end
 
-  def save_movie
-    movie = Movie.new(
-      title: self.title,
-      hotness: self.hotness,
-      image_url: self.image_url,
-      synopsis: self.synopsis,
-      rating: self.rating,
-      genre: self.genre,
-      director: self.director,
-      runtime: self.runtime
-      )
-    movie.save
-    if movie.errors
-      movie.errors.each { |error| puts error}
-    end
-  end
+  # def save_movie
+  #   movie = Movie.new(
+  #     title: self.title,
+  #     hotness: self.hotness,
+  #     image_url: self.image_url,
+  #     synopsis: self.synopsis,
+  #     rating: self.rating,
+  #     genre: self.genre,
+  #     director: self.director,
+  #     runtime: self.runtime
+  #     )
+  #   movie.save
+  #   if movie.errors
+  #     movie.errors.each { |error| puts error}
+  #   end
+  # end
 end
