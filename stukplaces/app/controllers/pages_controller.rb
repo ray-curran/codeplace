a412ecc6b928208617919766b03b24f6d8a87610
Ray Curran <rpcurr@gmail.com>
2016-09-28 13:09:52 -0500

    Configure dashboard

--- stukplaces/app/controllers/pages_controller.rb
+++ stukplaces/app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    if current_user 
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
+    @user = current_user
+    @places = @user.places 
+    @reviews = @user.reviews
   end
 end
