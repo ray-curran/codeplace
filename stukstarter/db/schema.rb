cd06c1f6478cb0bca102658b93af81012efc6375
Ray Curran <rpcurr@gmail.com>
2016-10-03 14:34:30 -0500

    Add navbar customized

--- stukstarter/db/schema.rb
+++ stukstarter/db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003192601) do
+ActiveRecord::Schema.define(version: 20161003193116) do
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "short_description"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "status",                                    default: "pending"
+    t.decimal  "goal",              precision: 8, scale: 2
+    t.datetime "expiration_date"
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
