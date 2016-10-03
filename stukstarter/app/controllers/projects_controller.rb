4ca8ca1323f8d731b4c9e7a1a56272af09b8edb4
Ray Curran <rpcurr@gmail.com>
2016-10-03 14:55:27 -0500

    Add html entries for project description

--- stukstarter/app/controllers/projects_controller.rb
+++ stukstarter/app/controllers/projects_controller.rb
@@ -20,8 +20,22 @@
 #
 
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @projects = Project.all
     @displayed_projects = Project.take(4)
   end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
 end
+
